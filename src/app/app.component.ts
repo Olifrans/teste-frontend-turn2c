9c2c0a31b0b73ba5325384ce1d7ebd3f50d03c23
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2023-08-14 12:36:50 -0300

    feta: Usando o component consumindo-api-dog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
 
-// Pode ser observado pelo observer
-// O retorno da API será inserido neste Observable
+// Recebe o retorno da API será inserido neste Observable
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,33 +11,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 
-//Implementação do OnInit
-export class AppComponent implements OnInit {
+
+export class AppComponent {
+
   title = 'teste-frontend-turn2c';
 
-  
-  // Variavél Observable que recebe o retorno da API, com padrão de notação Filandesa$
-  public thecatapi$!: Observable<any>;
-  // public pokemons$: Observable<any>;
-
-  // Construtor para injetar o HttpClient
-  constructor(private http: HttpClient) {}
-
-  //Importação do OnInit
-  ngOnInit(): void {
-    //Teste debug retorno da API
-    this.http
-      .get(' https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_ RNcTkLvQrnHKkidM2j1e5ucxnGS4mS WIiyUmA47kc4b3HrDXyAt1B1Tx9ngZ n1WM ')
-      .subscribe((fr) => console.log(fr));
-
-    //Faz requisição no BackEnd
-    this.thecatapi$ = this.http.get(
-      'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_ RNcTkLvQrnHKkidM2j1e5ucxnGS4mS WIiyUmA47kc4b3HrDXyAt1B1Tx9ngZ n1WM'     
-     
-    );
-
-
-    // this.thecatapi$ = this.http.post('url', { name: 'Olifrans' });
-    
-  }
 }
--- src/app/components/consumindo-api-dog/consumindo-api-dog.component.ts
+++ src/app/components/consumindo-api-dog/consumindo-api-dog.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+
+import { HttpClient } from '@angular/common/http';
+
+// Recebe o retorno da API será inserido neste Observable
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-consumindo-api-dog',
   templateUrl: './consumindo-api-dog.component.html',
   styleUrls: ['./consumindo-api-dog.component.css']
 })
-export class ConsumindoApiDogComponent {
+export class ConsumindoApiDogComponent implements OnInit {
+
+  
+  // Variavél Observable que recebe o retorno da API, com padrão de notação Filandesa$
+  public thecatapi$!: Observable<any>;
+  // public pokemons$: Observable<any>;
+
+  // Construtor para injetar o HttpClient
+  constructor(private http: HttpClient) {}
+
+  //Importação do OnInit
+  ngOnInit(): void {
+    //Teste debug retorno da API
+    this.http
+      .get(' https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_ RNcTkLvQrnHKkidM2j1e5ucxnGS4mS WIiyUmA47kc4b3HrDXyAt1B1Tx9ngZ n1WM ')
+      .subscribe((fr) => console.log(fr));
+
+    //Faz requisição no BackEnd
+    this.thecatapi$ = this.http.get(
+      'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_ RNcTkLvQrnHKkidM2j1e5ucxnGS4mS WIiyUmA47kc4b3HrDXyAt1B1Tx9ngZ n1WM'     
+     
+    );
+
+
+    // this.thecatapi$ = this.http.post('url', { id: 'PGDFRWCat' });
+    
+  }
+
+
+
+  
+
 
   
 }
